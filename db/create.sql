150547bc8959d584cd8acd18d44eceadd968f3df
Manuel Soto <sotomanuel41@icloud.com>
2018-09-19 13:03:58 -0500

    tried doing the "schema"

--- db/create.sql
+++ db/create.sql
@@ -1,4 +1,4 @@
-create table if not exists users(
+CREATE TABLE IF NOT EXISTS users(
     idUser integer primary key asc,
     studentID integer,
     username text,
@@ -7,4 +7,21 @@ create table if not exists users(
     first text,
     last text,
     userLevel integer default 2
+);
+
+CREATE TABLE IF NOT EXISTS movies(
+    movieID INTEGER PRIMARY KEY ASC,
+    movie_name TEXT NOT NULL,
+    relase_date NUMERIC,
+    movie_genre TEXT NOT NULL,
+    movie_rating INTEGER,
+
+);
+
+create table if not exists awards(
+    
+);
+
+create table if not exists movies(
+    
 );
