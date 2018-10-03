c286e27f881fbcf8bee9530c1c566affb55b8752
Frank Orechio <frank@technologyoneusa.com>
2018-10-03 10:28:43 -0500

    Schema created

--- db/create.sql
+++ db/create.sql
@@ -1,27 +1,83 @@
-CREATE TABLE IF NOT EXISTS users(
-    idUser integer primary key asc,
-    studentID integer,
-    username text,
-    password text,
-    salt text,
-    first text,
-    last text,
-    userLevel integer default 2
-);
-
-CREATE TABLE IF NOT EXISTS movies(
-    movieID INTEGER PRIMARY KEY ASC,
-    movie_name TEXT NOT NULL,
-    relase_date NUMERIC,
-    movie_genre TEXT NOT NULL,
-    movie_rating INTEGER,
-
-);
-
-create table if not exists awards(
-    
-);
-
 create table if not exists movies(
-    
+    id_movie integer primary key asc,
+    name text not null,
+    releasedate datetime not null,
+    moviegenre text not null,
+    movierating integer not null
+);
+create table if not exists users(
+    id_user integer primary key asc,
+    firstname text not null,
+    lastname text not null,
+    dateofbirth datetime not null
+);
+create table if not exists actors(
+    id_actor integer not null,
+    firstname text not null,
+    lastname text not null
+);
+create table if not exists writers(
+    id_writer integer primary key asc,
+    firstname text not null,
+    lastname text not null
+);
+create table if not exists awards(
+    id_award integer primary key asc,
+    awardname text not null,
+    awardsdate datetime not null,
+    id_movie integer not null,
+    foreign key id_movie references movies(id_movie) on delete cascade
+);
+create table if not exists directors(
+    id_director integer primary key asc,
+    firstname text not null,
+    lastname text not null,
+);
+create table if not exists production(
+    id_production integer primary key asc,
+    productionname text not null
+);
+create table if not exists actcred(
+    id_credits integer not null,
+    id_actor integer not null,
+    id_movie integer not null,
+    foreign key id_actor references actors(id_actor) on delete cascade,
+    foreign key id_movie references movies(id_movie) on delete cascade
+);
+create table if not exists dircred(
+    id_dircred integer primary key asc,
+    id_movie integer not null,
+    id_director integer not null,
+    foreign key id_movie references movies(id_movie) on delete cascade,
+    foreign key id_director references directors(id_director) on delete cascade
+);
+create table if not exists prodcred(
+    id_prodcred integer primary key asc,
+    id_movie integer not null,
+    id_production integer not null,
+    foreign key id_movie references movies(id_movie) on delete cascade,
+    foreign key id_production references production(id_production) on delete cascade
+);
+create table if not exists writercred(
+    id_writercred integer primary key asc,
+    id_movie integer not null,
+    id_writer integer not null,
+    foreign key id_movie references movies(id_movie) on delete cascade,
+    foreign key id_writer references writers(id_writer) on delete cascade
+);
+create table if not exists awardcred(
+    id_actcred integer primary key asc,
+    id_movie integer not null,
+    id_award integer not null,
+    foreign key id_movie references movies(id_movie) on delete cascade,
+    foreign key id_award references awards(id_award) on delete cascade
+);
+create table if not exists usercomment(
+    id_usercomment integer not null,
+    id_user integer not null,
+    id_movie integer not null,
+    movierating integer not null,
+    usercomment text null,
+    foreign key id_movie references movies(id_movie) on delete cascade,
+    foreign key id_user references users(id_user) on delete cascade
 );
